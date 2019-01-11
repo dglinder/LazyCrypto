cecc527ed0d6d254425aeceb3485d9fe3a0cb6bf
Daniel Linder <dglinder@west.com>
2019-01-10 19:35:28 -0600

    Blank space cleanup and prevent overwiting a file.

--- lazy_crypto.sh
+++ lazy_crypto.sh
@@ -2,46 +2,52 @@
 ###################################################
 # Lazy Crypto
 # Written By: Captain Bossman
-# 
-# Eases the pain of encrypting and decrypting 
-# individual files using OpenSSL.  
+#
+# Eases the pain of encrypting and decrypting
+# individual files using OpenSSL.
 #
 # Please do not be alarmed that this is a quick and
 # dirty hack.  Feel free to use this for anything
-# if you happen to find it useful. 
-# 
-# Please direct any support requests to 
+# if you happen to find it useful.
+#
+# Please direct any support requests to
 # http://www.google.com
 ####################################################
 set -u
 set -e
-# set -x  # Un-comment to show debug output - SHOWS PASSWORD ON SCREEN
+#set -x
 
-# Encryption algorhythm to use. 
-# Check 'openssl enc help' for options. 
- 
-algo="aes-256-cbc" 
- 
-# Panic if we don't have the right number of 
-# arguments 
+# Encryption algorhythm to use.
+# Check 'openssl enc help' for options.
+
+algo="aes-256-cbc"
+
+# Panic if we don't have the right number of
+# arguments
 set +u
-if [ "$2" == "" ]; then 
+if [ "$2" == "" ]; then
     echo "Usage: $0 [ encrypt|decrypt ] filename"
-    exit 2 
+    exit 2
 else
     set -u
-    echo -n "Passphrase: " 
-    stty -echo   
+    echo -n "Passphrase: "
+    stty -echo
     read passwd
     stty echo
     echo
- 
-    case "$1" in 
+
+    case "$1" in
         encrypt)
-            openssl enc -$algo -e -in $2 -base64 -out $2.enc -pass pass:"${passwd}"; 
+            openssl enc -$algo -e -in "$2" -base64 -out "$2.enc" -pass pass:"${passwd}";
         ;;
         decrypt)
-            openssl enc -$algo -d -in $2 -base64 -out `echo $2 | sed -e 's/\.enc//'` -pass pass:"${passwd}";
+            OUTF=$(echo "$2" | sed -e 's/\.enc//')
+            if [[ -e "${OUTF}" ]] ; then
+              echo -n "Keeping found file named \"${OUTF}\"."
+              OUTF="${OUTF}.$(date +%s)"
+              echo "Writing to \"${OUTF}\" instead."
+            fi
+            openssl enc -$algo -d -in "$2" -base64 -out "${OUTF}" -pass pass:"${passwd}";
         ;;
 
         *)
